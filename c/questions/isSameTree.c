0b0ce17b7973b49a34896f2a1baf518be9c98e27
ocean <jojoceanus@outlook.com>
2026-01-08 23:13:05 +0800

    leetcode 100 isSameTree

---
+++ c/questions/isSameTree.c
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
+    if (p && q) {
+        if (p->val == q->val) {
+            return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+        }
+        return false;
+    }
+    if (!p && !q) {
+        return true;
+    }
+    return false;
+}
