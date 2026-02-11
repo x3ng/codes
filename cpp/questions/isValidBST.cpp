44d3ce81649afc4e81fd0fc3ef257dbb48651456
xen <jojoceanus@outlook.com>
2026-02-11 21:07:35 +0800

    leetcode 98 isValidBST

---
+++ cpp/questions/isValidBST.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    long long pre = LLONG_MIN;
+public:
+    bool isValidBST(TreeNode* root) {
+        if (root == nullptr) {
+            return true;
+        }
+        if (!isValidBST(root->left)) {
+            return false;
+        }
+        if (root->val <= pre) {
+            return false;
+        }
+        pre = root->val;
+        return isValidBST(root->right);
+    }
+};
