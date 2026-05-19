097386294066be3cb400b963783aeb08ad839e4f
xen <jojoceanus@outlook.com>
2026-05-19 14:21:30 +0800

    leetcode 189 rotate

---
+++ cpp/questions/rotate1.cpp
@@ -0,0 +1,15 @@
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k %= n;
+        std::reverse(nums.begin(), nums.end());
+        std::reverse(nums.begin(), nums.begin()+k);
+        std::reverse(nums.begin()+k, nums.end());
+    }
+};
---
+++ python/questions/rotate1.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        def reverse(i: int, j: int) -> None:
+            while i < j:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+                j -= 1
+        k %= len(nums)
+        reverse(0, len(nums)-1)
+        reverse(0, k-1)
+        reverse(k, len(nums)-1)
