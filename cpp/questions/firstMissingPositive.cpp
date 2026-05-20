d4957b70c03102ee3d82ac35625ace887d188d5a
xen <jojoceanus@outlook.com>
2026-05-20 12:25:32 +0800

    leetcode 41 firstMissingPostive

---
+++ cpp/questions/firstMissingPositive.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+        for (int i=0; i<n; ++i) {
+            while (nums[i]>0 && nums[i]<n && nums[i]!=i+1 && nums[nums[i]-1]!=nums[i]) {
+                std::swap(nums[i], nums[nums[i]-1]);
+            }
+        }
+        for (int i=0; i<n; ++i) {
+            if (nums[i] != i+1) {
+                return i+1;
+            }
+        }
+        return n+1;
+    }
+};
