a3d794c36a26ead73b5e188cb1a99cba0033092a
xen <jojoceanus@outlook.com>
2026-06-01 09:56:54 +0800

    leetcode 48 rotate

--- cpp/questions/rotate.cpp
+++ cpp/questions/rotate.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <vector>
 
 using namespace std;
@@ -6,15 +7,11 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        int l = 0;
-        for (int l=0; l<n/2; ++l) {
-            for (int p=l; p<n-1-l; ++p) {
-                int tmp = matrix[l][p];
-                matrix[l][p] = matrix[n-1-p][l];
-                matrix[n-1-p][l] = matrix[n-1-l][n-1-p];
-                matrix[n-1-l][n-1-p] = matrix[p][n-1-l];
-                matrix[p][n-1-l] = tmp;
+        for (int i=0; i<n; ++i) {
+            for (int j=i+1; j<n; ++j) {
+                std::swap(matrix[i][j], matrix[j][i]);
             }
+            std::reverse(matrix[i].begin(), matrix[i].end());
         }
     }
 };
