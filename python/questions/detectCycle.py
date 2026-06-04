4eccb65b5b3566eb3d9dc4b44cefe778df32b098
xen <jojoceanus@outlook.com>
2026-06-04 11:23:29 +0800

    leetcode 142 detectCycle

---
+++ python/questions/detectCycle.py
@@ -0,0 +1,20 @@
+from typing import Optional
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                while head != fast:
+                    head = head.next
+                    fast = fast.next
+                return head
+        return None
