e2b4ea8855a0207b1b4eaeeccda38bc74e1bd3a7
xen <jojoceanus@outlook.com>
2026-06-04 13:15:41 +0800

    leetcode 2 addTwoNumbers

--- cpp/questions/addTwoNumbers.cpp
+++ cpp/questions/addTwoNumbers.cpp
@@ -1,48 +1,36 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
 class Solution {
-private:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2, int carry) {
-        if (l1 && l2) {
-            l1->val = l1->val + l2->val + carry;
-            carry = l1->val / 10;
-            l1->val %= 10;
-            l1->next = addTwoNumbers(l1->next, l2->next, carry);
-            return l1;
-        } else {
-            if (carry) {
-                if (l1) {
-                    l1->val = l1->val + carry;
-                    carry = l1->val / 10;
-                    l1->val %= 10;
-                    l1->next = addTwoNumbers(l1->next, nullptr, carry);
-                    return l1;
-                } else if (l2) {
-                    l2->val = l2->val + carry;
-                    carry = l2->val / 10;
-                    l2->val %= 10;
-                    l2->next = addTwoNumbers(l2->next, nullptr, carry);
-                    return l2;
-            
-                } else {
-                    return new ListNode(1);
-                }
-            } else {
-                return l1 ? l1 : l2;
-            }
-        }
-    }
-    
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        return addTwoNumbers(l1, l2, 0);
+        bool carry = false;
+        ListNode dummy{};
+        dummy.next = l1;
+        ListNode* cur = &dummy;
+        while (l1 && l2) {
+            l1->val += l2->val + carry;
+            carry = l1->val / 10;
+            l1->val %= 10;
+            cur = l1;
+            l1 = l1->next;
+            l2 = l2->next;
+        }
+        cur->next = l1 ? l1 : l2;
+        while (carry && cur->next) {
+            cur = cur->next;
+            cur->val += 1;
+            carry = cur->val / 10;
+            cur->val %= 10;
+        }
+        if (carry) {
+            cur->next = new ListNode(1, nullptr);
+        }
+        return dummy.next;
     }
 };
--- python/questions/addTwoNumbers.py
+++ python/questions/addTwoNumbers.py
@@ -1,23 +1,28 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 class Solution:
-    def addTwoNumbersCarry(self, l1:Optional[ListNode], l2: Optional[ListNode], carry: int) -> Optional[ListNode]:
-        if l1 is None and l2 is None:
-            return carry ? ListNode(1) : None
-        if l1 and l2:
-            carry = carry + l1.val + l2.val
-            l1.val = carry % 10
-            l1.next = self.addTwoNumbersCarry(l1.next, l2.next, carry//10)
-            return l1
-        if l2:
-            l1 = l2
-        if carry:
-            carry = carry + l1.val
-            l1.val = carry % 10
-            l1.next = self.addTwoNumbersCarry(l1.next, None, carry//10)
-        return l1
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        return self.addTwoNumbersCarry(l1, l2, 0)
+        dummy = ListNode(0, l1)
+        cur = dummy
+        carry = 0
+        while l1 and l2:
+            l1.val += l2.val + carry
+            carry = l1.val // 10
+            l1.val %= 10
+            cur = l1
+            l1 = l1.next
+            l2 = l2.next
+        cur.next = l1 if l1 else l2
+        while cur.next and carry!=0:
+            cur = cur.next
+            cur.val += 1
+            carry = cur.val // 10
+            cur.val %= 10
+        if carry == 1:
+            cur.next = ListNode(1, None)
+        return dummy.next
