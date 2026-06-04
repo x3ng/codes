fe7b41bbde260f70e29b6630dd763e63b10abbbe
xen <jojoceanus@outlook.com>
2026-06-04 11:36:26 +0800

    leetcode 21 mergeTwoList

--- cpp/questions/mergeTwoLists.cpp
+++ cpp/questions/mergeTwoLists.cpp
@@ -1,30 +1,27 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         ListNode dummy;
-        ListNode* current = &dummy;
-        dummy->next = current;
+        ListNode* cur = &dummy;
         while (list1 && list2) {
-            if (list1->val<list2->val) {
-                current->next = list1;
-                list1 = list1->next;
-            } else {
-                current->next = list2;
+            if (list1->val > list2->val) {
+                cur->next = list2;
                 list2 = list2->next;
+            } else {
+                cur->next = list1;
+                list1 = list1->next;
             }
-            current = current->next;
+            cur = cur->next;
         }
-        current->next = list1 ? list1 : list2;
+        cur->next = list1 ? list1 : list2;
         return dummy.next;
     }
 };
--- python/questions/mergeTwoLists.py
+++ python/questions/mergeTwoLists.py
@@ -1,17 +1,22 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        if list1 is None:
-            return list2
-        if list2 is None:
-            return list1
-        if list1.val < list2.val:
-            list1.next = self.mergeTwoLists(list1.next, list2)
-            return list1
-        else:
-            list2.next = self.mergeTwoLists(list1, list2.next)
-            return list2
+        dummy = ListNode()
+        cur = dummy
+        while list1 and list2:
+            if list1.val > list2.val:
+                cur.next = list2
+                list2 = list2.next
+            else:
+                cur.next = list1
+                list1 = list1.next
+            cur = cur.next
+        cur.next = list1 if list1 else list2
+        return dummy.next
+
