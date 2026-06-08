a4b51a1486aed40c486f749f6e2f7c8278039df3
xen <jojoceanus@outlook.com>
2026-06-08 18:40:02 +0800

    leetcode 138 copyRandomList

--- cpp/questions/copyRandomList.cpp
+++ cpp/questions/copyRandomList.cpp
@@ -1,5 +1,6 @@
-/*
-// Definition for a Node.
+//#include <bits/stdc++.h>
+#include <unordered_map>
+
 class Node {
 public:
     int val;
@@ -8,42 +9,29 @@ public:
     
     Node(int _val) {
         val = _val;
-        next = NULL;
-        random = NULL;
+        next = nullptr;
+        random = nullptr;
     }
 };
-*/
 
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        if (head == nullptr) {
-            return nullptr;
+        std::unordered_map<Node*, Node*> hm; 
+        Node* dummy = new Node(0);
+        Node* cur = dummy;
+        while (head) {
+            cur->next = new Node(head->val);
+            hm[head] = cur->next;
+            cur = cur->next;
+            cur->random = head->random;
+            head = head->next;
         }
-        Node* curr = head;
-        while (curr) {
-            Node* inst = new Node(curr->val);
-            inst->next = curr->next;
-            curr->next = inst;
-            curr = inst->next;
+        cur = dummy->next;
+        while (cur) {
+           cur->random = hm[cur->random];
+           cur = cur->next;
         }
-        curr = head;
-        Node* tmp;
-        while (curr) {
-            tmp = curr->next;
-            tmp->random = curr->random==nullptr ? nullptr : curr->random->next;
-            curr = tmp->next;
-        }
-        Node* ans = head->next;
-        curr = head;
-        tmp = head->next;
-        while (tmp->next) {
-            curr->next = tmp->next;
-            tmp->next = curr->next->next;
-            curr = curr->next;
-            tmp = tmp->next;
-        }
-        curr->next = nullptr;
-        return ans;
+        return dummy->next;
     }
 };
--- python/questions/copyRandomList.py
+++ python/questions/copyRandomList.py
@@ -1,22 +1,22 @@
-"""
-# Definition for a Node.
+from typing import Optional
+
 class Node:
     def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
         self.val = int(x)
         self.next = next
         self.random = random
-"""
 
 class Solution:
-    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        nmp = {}
-        curr = head
-        while curr:
-            nmp[curr] = Node(curr.val)
-            curr = curr.next
-        curr = head
-        while curr:
-            nmp[curr].next = nmp.get(curr.next)
-            nmp[curr].random = nmp.get(curr.random)
-            curr = curr.next
-        return nmp.get(head)
+    def copyRandomList(self, head: Optional[Node]) -> Optional[Node]:
+        dt = {}
+        dt[None] = None
+        cur = head
+        while cur is not None:
+            dt[cur] = Node(cur.val)
+            cur = cur.next
+        cur = head
+        while cur is not None:
+            dt[cur].next = dt[cur.next]
+            dt[cur].random = dt[cur.random]
+            cur = cur.next
+        return dt[head]
