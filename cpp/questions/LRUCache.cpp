01b1006626f5b756a55a95ec69f02e2a71624f02
xen <jojoceanus@outlook.com>
2026-06-09 17:44:46 +0800

    leetcode 146 LRUCache

--- cpp/questions/LRUCache.cpp
+++ cpp/questions/LRUCache.cpp
@@ -1,50 +1,69 @@
+// #include <bits/stdc++.h>
+#include <unordered_map>
+
+using namespace std;
+
 class LRUCache {
 private:
-    std::unordered_map<int, std::pair<int, int>> hvm;
-    std::queue<int> kq;
+    struct Node {
+        int key;
+        int val;
+        Node* pre;
+        Node* nxt;
+
+        Node(int k, int v, Node* p, Node* n) : key(k), val(v), pre(p), nxt(n) {}
+    };
+
+    Node* head;
+    Node* tail;
+    std::unordered_map<int, Node*> hm;
     int cap;
+
+    void freshNode(Node* n) {
+        n->nxt->pre = n->pre;
+        n->pre->nxt = n->nxt;
+        n->nxt = head->nxt;
+        n->pre = head;
+        n->nxt->pre = n;
+        head->nxt = n;
+    }
+
 public:
     LRUCache(int capacity) {
         cap = capacity;
+        head = new Node(0, 0, nullptr, nullptr);
+        tail = new Node(0, 0, head, nullptr);
+        head->nxt = tail;
     }
     
     int get(int key) {
-        auto it = hvm.find(key);
-        if (it != hvm.end()) {
-            put(key, it->second.second);
-            return hvm[key].second;
+        auto it = hm.find(key);
+        if (it != hm.end()) {
+            Node* cn = it->second;
+            freshNode(cn);
+            return cn->val;
         } else {
             return -1;
         }
     }
     
     void put(int key, int value) {
-        if (hvm.find(key) != hvm.end()) {
-            hvm[key].first += 1;
-            hvm[key].second = value;
-            cap++;
-            kq.push(key);
-            return;
+        if (get(key) != -1) {
+            head->nxt->val = value;
+        } else if (cap) {
+            --cap;
+            Node* nn = new Node(key, value, head, head->nxt);
+            head->nxt->pre = nn;
+            head->nxt = nn;
+            hm[key] = nn;
+        } else {
+            Node* on = tail->pre;
+            int ok = on->key;
+            hm.erase(ok);
+            hm[key] = on;
+            freshNode(on);
+            on->key = key;
+            on->val = value;
         }
-        while (kq.size() == cap) {
-            int dk = kq.front();
-            if (hvm[dk].first == 1) {
-                hvm.erase(dk);
-            } else {
-                hvm[dk].first--;
-                cap--;
-            }
-            kq.pop();
-        }
-        kq.push(key);
-        hvm[key] = {1, value};
     }
-    
 };
-
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache* obj = new LRUCache(capacity);
- * int param_1 = obj->get(key);
- * obj->put(key,value);
- */
--- python/questions/LRUCache.py
+++ python/questions/LRUCache.py
@@ -1,59 +1,51 @@
+from __future__ import annotations
 from typing import Optional
 
 class LRUCache:
 
-    class DualListNode:
-        def __init__(self, val: int=0, prev: Optional["DualListNode"]=None, next: Optional["DualListNode"]=None):
-            self.val = val
-            self.prev = prev
-            self.next = next
+    class Node:
+        def __init__(self, k: int, v: int, p: Optional[Node], n: Optional[Node]):
+            self.k = k
+            self.v = v
+            self.p = p
+            self.n = n
 
     def __init__(self, capacity: int):
-        self.cap = capacity
-        self.kvm = {}
-        self.knm = {}
-        self.lnh = self.DualListNode(0)
-        self.lnt = self.DualListNode(0)
-        self.lnh.next = self.lnt
-        self.lnt.prev = self.lnh
+        self.cap = capacity        
+        self.dt = {}
+        self.h = self.Node(0, 0, None, None)
+        self.t = self.Node(0, 0, self.h, None)
+        self.h.n = self.t
+
+    def _freshNode(self, n: Node) -> None:
+        n.n.p = n.p
+        n.p.n = n.n
+        n.p = self.h
+        n.n = self.h.n
+        n.n.p = n
+        self.h.n = n
 
     def get(self, key: int) -> int:
-        if key in self.kvm:
-            cn = self.knm[key]
-            cn.prev.next = cn.next
-            cn.next.prev = cn.prev
-            self.lnt.prev.next = cn
-            cn.prev = self.lnt.prev
-            cn.next = self.lnt
-            self.lnt.prev = cn
-            return self.kvm[key]
-        else:
+        cn = self.dt.get(key, None) 
+        if cn is None:
             return -1
+        self._freshNode(cn)
+        return cn.v
 
     def put(self, key: int, value: int) -> None:
         if self.get(key) != -1:
-            self.kvm[self.lnt.prev.val] = value
-            return
-        if len(self.kvm) < self.cap:
-            nn = self.DualListNode(key, self.lnt.prev, self.lnt)
-            self.lnt.prev.next = nn
-            self.lnt.prev = nn
-            self.kvm[key] = value
-            self.knm[key] = nn
+            self.h.n.v = value
+        elif self.cap:
+            self.cap -= 1
+            nn = self.Node(key, value, self.h, self.h.n)
+            nn.n.p = nn
+            self.h.n = nn
+            self.dt[key] = nn
         else:
-            mn = self.lnh.next
-            self.lnh.next = mn.next
-            mn.next.prev = self.lnh
-            mn.next = self.lnt
-            mn.prev = self.lnt.prev
-            self.lnt.prev.next = mn
-            self.lnt.prev = mn
-            self.knm[key] = self.knm.pop(mn.val)
-            self.kvm.pop(mn.val)
-            self.kvm[key] = value
-            mn.val = key
-
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
+            on = self.t.p
+            self._freshNode(on)
+            ok = on.k
+            self.dt.pop(ok)
+            self.dt[key] = on
+            on.k = key
+            on.v = value
