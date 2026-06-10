e789737fb55ea3b26919e1f2190f421c78636759
xen <jojoceanus@outlook.com>
2026-06-10 12:55:46 +0800

    leetcode 101 isSymmetric

--- cpp/questions/isSymmetric.cpp
+++ cpp/questions/isSymmetric.cpp
@@ -1,32 +1,26 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+#include <functional>
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
 class Solution {
-private:
-    bool symmetricLR(TreeNode* left, TreeNode* right) {
-        if (left && right) {
-            if (left->val == right->val) {
-                return symmetricLR(left->left, right->right) && symmetricLR(left->right, right->left);
-            }
-        }
-        if (!left && !right) {
-            return true;
-        }
-        return false;
-    }
 public:
     bool isSymmetric(TreeNode* root) {
-        if (root) {
-            return symmetricLR(root->left, root->right);
-        }
-        return false;
+        std::function<bool(TreeNode*, TreeNode*)> cLR = [&](TreeNode* l, TreeNode* r) -> bool {
+            if (l && r) {
+                return l->val==r->val ? cLR(l->left, r->right) && cLR(l->right, r->left) : false;
+            }
+            if (l==nullptr && r == nullptr) {
+                return true;
+            }
+            return false;
+        };
+        return root ? cLR(root->left, root->right) : true;
     }
 };
--- python/questions/isSymmetric.py
+++ python/questions/isSymmetric.py
@@ -1,19 +1,17 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        def symmetricLR(left: Optional[TreeNode], right: Optional[TreeNode]) -> bool:
-            if left and right:
-                if left.val == right.val:
-                    return symmetricLR(left.left, right.right) and symmetricLR(left.right, right.left)
-                return False
-            if not left and not right:
+        def cLR(l: Optional[TreeNode], r: Optional[TreeNode]) -> bool:
+            if l and r:
+                return cLR(l.left, r.right) and cLR(l.right, r.left) if l.val==r.val else False
+            if l is None and r is None:
                 return True
             return False
-        if not root:
-            return True
-        return symmetricLR(root.left, root.right)
+        return cLR(root.left, root.right) if root else True
