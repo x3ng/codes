f7449b80699e2d37463716f078ac7d6a9d7f8b89
xen <jojoceanus@outlook.com>
2026-06-14 14:52:37 +0800

    leetcode 105 buildTree

--- python/questions/buildTree.py
+++ python/questions/buildTree.py
@@ -1,18 +1,24 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+from typing import Optional, List
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        def build(pres: int, ins: int, tlen: int) -> Optional[TreeNode]:
-            if tlen == 0:
-                return None
-            ip = 0
-            for ip in range(ins, ins+tlen):
-                if inorder[ip] == preorder[pres]:
-                    break
-            ll = ip - ins
-            return TreeNode(inorder[ip], build(pres+1, ins, ip-ins), build(pres+ll+1, ip+1, ins+tlen-ip-1))
-        return build(0, 0, len(inorder))
+        pp = 0
+        dt = {}
+        for i in range(len(inorder)):
+            dt[inorder[i]] = i
+        def bt(il: int, ir: int) -> Optional[TreeNode]:
+            nonlocal pp, dt
+            nv = preorder[pp];
+            pp += 1;
+            tn = TreeNode(nv)
+            ip = dt[nv]
+            tn.left = None if ip==il else bt(il, ip-1)
+            tn.right = None if ip==ir else bt(ip+1, ir)
+            return tn
+        return bt(0, len(inorder)-1)
