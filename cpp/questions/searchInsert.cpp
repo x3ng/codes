a5589e2be8a9cef40998376a95fa905e15433814
xen <jojoceanus@outlook.com>
2026-06-25 10:32:10 +0800

    leetcode 35 searchInsert

--- cpp/questions/searchInsert.cpp
+++ cpp/questions/searchInsert.cpp
@@ -1,19 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
         int l = 0;
         int r = nums.size() - 1;
+        // if (nums[l] >= target) {
+        //     return 0;
+        // } else if (nums[r] == target) {
+        //     return r;
+        // } else if (nums[r] < target) {
+        //     return nums.size();
+        // }
         while (l < r) {
             int m = (l + r) / 2;
-            int dif = nums[m] - target;
-            if (dif == 0) {
+            if (nums[m] == target) {
                 return m;
-            } else if (dif > 0) {
+            } else if (nums[m] > target) {
                 r = m - 1;
             } else {
                 l = m + 1;
             }
         }
-        return nums[l] >= target ? l : l+1;
+        return nums[l]<target ? l+1 : l;
     }
 };
---
+++ python/questions/searchInsert.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        l = 0
+        r = len(nums)
+        while l < r:
+            m = (l + r - 1) // 2
+            if nums[m] == target:
+                return m
+            elif nums[m] > target:
+                r = m
+            else:
+                l = m + 1
+        return l
