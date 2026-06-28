c5137ba8c24a1590ec0fe256301db867ff5f991b
xen <jojoceanus@outlook.com>
2026-06-28 13:38:23 +0800

    leetcode 33 search

--- cpp/questions/search.cpp
+++ cpp/questions/search.cpp
@@ -1,33 +1,27 @@
-#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int ns = nums.size();
-        int nev = nums[ns-1];
-        if (target == nev) {
-            return ns-1;
-        }
         int l = 0;
-        int r = ns - 1;
+        int r = nums.size() - 1;
         while (l < r) {
             int m = (l + r) / 2;
-            int mv = nums[m];
-            if (mv == target) {
+            if (nums[m] == target) {
                 return m;
-            } else if (target > nev) {
-                if (nums[m]<nev || nums[m]>target) {
-                    r = m - 1;
-                } else {
+            } else if (nums[m] > target) {
+                if (nums[m]>=nums[l] && nums[l]>target) {
                     l = m + 1;
+                } else {
+                    r = m - 1;
                 }
-            } else if (target < nev) {
-                if (nums[m]>nev || nums[m]<target) {
-                    l = m + 1;
-                } else {
+            } else {
+                if (nums[m]<nums[l] && nums[r]<target) {
                     r = m - 1;
+                } else {
+                    l = m + 1;
                 }
             }
         }
--- python/questions/search.py
+++ python/questions/search.py
@@ -2,14 +2,20 @@ from typing import List
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        r = len(nums) - 1
         l = 0
+        r = len(nums) - 1
         while l < r:
             m = (l + r) // 2
             if nums[m] == target:
                 return m
-            elif nums[m] > target:
-                r = m - 1
-            else:
-                l = m + 1
+            elif nums[m]<target:
+                if nums[m]<nums[l] and nums[r]<target:
+                    r = m - 1
+                else:
+                    l = m + 1
+            elif nums[m]>target:
+                if nums[m]>=nums[l] and nums[l]>target:
+                    l = m + 1
+                else:
+                    r = m - 1
         return l if nums[l]==target else -1
