5ed2f7fe170a44346ca8a49a02c751e52512e64c
xen <jojoceanus@outlook.com>
2026-07-22 12:42:37 +0800

    leetcode 287 findDuplicate

---
+++ python/questions/findDuplicate.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        slow = 0
+        fast = 0
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+        slow = 0
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+        return slow
