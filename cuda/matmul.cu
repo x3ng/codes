fa87af8fbe9847ca6356d743396d7681bf95bcb4
xen <jojoceanus@outlook.com>
2026-09-13 07:47:52 +0800

    update matmul.cu

--- cuda/matmul.cu
+++ cuda/matmul.cu
@@ -1,6 +1,7 @@
-#include "cudaCheck.h"
+#include "cuda_check.h"
+#include <assert.h>
 
-__global__ void matmul(float* A, float* B, float* C, int M, int N, int K) {
+__global__ void matmul(float *A, float *B, float *C, int M, int N, int K) {
   assert(blockDim.x == blockDim.y);
   const int TS = blockDim.x;
   const int MO = TS * TS;
@@ -8,16 +9,22 @@ __global__ void matmul(float* A, float* B, float* C, int M, int N, int K) {
   int row = blockIdx.y * TS + threadIdx.y;
   int col = blockIdx.x * TS + threadIdx.x;
   float cv = 0.0f;
-  for (int s=0; s<(K+TS-1)/TS; ++s) {
-    mem[threadIdx.y*TS+threadIdx.x] = row<M&&(s*TS+threadIdx.x)<K ? A[row*K+s*TS+threadIdx.x] : 0.0f;
-    mem[MO+threadIdx.y*TS+threadIdx.x] = (s*TS+threadIdx.y)<K&&col<N ? B[(s*TS+threadIdx.y)*N+col] : 0.0f;
+  for (int s = 0; s < (K + TS - 1) / TS; ++s) {
+    mem[threadIdx.y * TS + threadIdx.x] =
+        row < M && (s * TS + threadIdx.x) < K
+            ? A[row * K + s * TS + threadIdx.x]
+            : 0.0f;
+    mem[MO + threadIdx.y * TS + threadIdx.x] =
+        (s * TS + threadIdx.y) < K && col < N
+            ? B[(s * TS + threadIdx.y) * N + col]
+            : 0.0f;
     __syncthreads();
-    for (int t=0; t<TS; ++t) {
-        cv += mem[threadIdx.y*TS+t] * mem[MO+t*TS+threadIdx.x];
+    for (int t = 0; t < TS; ++t) {
+      cv += mem[threadIdx.y * TS + t] * mem[MO + t * TS + threadIdx.x];
     }
-  }  
-  if (row<M && col<N) {
-    C[row*N+col] = cv;
+  }
+  if (row < M && col < N) {
+    C[row * N + col] = cv;
   }
 }
 
@@ -27,19 +34,19 @@ int main() {
   int N = 512;
   int tile_size = 32;
 
-  float* A;
-  cudaCheck(cudaMalloc(&A, M*K*sizeof(float)));
-  float* B;
-  cudaCheck(cudaMalloc(&B, K*N*sizeof(float)));
-  float* C;
-  cudaCheck(cudaMalloc(&C, M*N*sizeof(float)));
+  float *A;
+  CUDA_CHECK(cudaMalloc(&A, M * K * sizeof(float)));
+  float *B;
+  CUDA_CHECK(cudaMalloc(&B, K * N * sizeof(float)));
+  float *C;
+  CUDA_CHECK(cudaMalloc(&C, M * N * sizeof(float)));
 
   dim3 block(tile_size, tile_size);
-  dim3 grid((N+block.x-1)/block.x, (M+block.y-1)/block.y);
+  dim3 grid((N + block.x - 1) / block.x, (M + block.y - 1) / block.y);
   size_t sm = 2 * tile_size * tile_size * sizeof(float);
 
   matmul<<<grid, block, sm>>>(A, B, C, M, N, K);
-  cudaCheck(cudaDeviceSynchronize());
+  CUDA_CHECK(cudaDeviceSynchronize());
 
   cudaFree(A);
   cudaFree(B);
